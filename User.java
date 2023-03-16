40413695d8869e478a8fc04c1be63d2cff1033ba
ngyh6726 <61657888+ngyh6726@users.noreply.github.com>
2023-03-16 23:08:02 +0800

    Update User.java

--- User.java
+++ User.java
@@ -1,21 +1,32 @@
 public abstract class User {
     protected String userID;
     protected String password;
+    protected int authenticated;
 
     public User(String userID) {
         this.userID = userID;
         this.password = "password";
+        this.authenticated = 0;
     }
 
-    public boolean authenticate(String password) {
-        return this.password.equals(password);
+    public void login(String password) {
+        if(password == this.password){
+            this.authenticated = 1;
+            System.out.println("You have logged in successfully");
+        else
+            System.out.println("Wrong password, please try again");
+        return;
     }
 
     public void changePassword(String newPassword) {
-        this.password = newPassword;
+        if(this.authenticated == 1){
+            this.password = newPassword;
+            System.out.println("Your password has been successfully changed!");
+        }
+        else
+            System.out.println("You are not authenticated! Please log in to change your password");
+        return;
     }
-
-    public abstract String getType();
 
     public String getUserID() {
         return userID;
